60d34bf5b59e590031fdec12a771b8334ae0dbf9
Thomas Whittle <tewhittle@gmail.com>
2021-01-12 17:13:43 -0500

    Add delete route functionality

--- server.js
+++ server.js
@@ -13,14 +13,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-
 //HTML routes
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
 })
 
 
@@ -32,13 +31,42 @@ app.get("/api/notes", function(req, res)
 })
 
 app.post("/api/notes", function(req, res) 
- {
+ {  
+    
     const notesList = JSON.parse(fs.readFileSync("./db/db.json"));
     const postedNote = req.body;
+    postedNote.id = 0
+    function randomId() {
+        
+        id = Math.floor(Math.random() * Math.floor(1000))
+        notesList.forEach(index => {
+            if(index.id === id) {
+                randomId()
+            } else {
+               postedNote.id = id 
+            }
+        })
+    }
+    randomId()
     notesList.push(postedNote);
     fs.writeFileSync("./db/db.json", JSON.stringify(notesList))
     res.json(notesList);
     
+})
+
+app.delete("/api/notes/:id", function(req, res) 
+{
+    const notesList = JSON.parse(fs.readFileSync("./db/db.json"));
+    let updatedNotes = []
+    for (var i = 0; i < notesList.length; i++ ) 
+    {
+        if(notesList[i].id != req.params.id) 
+        {
+            updatedNotes.push(notesList[i])
+        }
+    }
+    fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes));
+    res.json(updatedNotes);
 })
 
 //Server listen
