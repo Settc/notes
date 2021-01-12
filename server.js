9a1493cd650af4d23ee262c4ccaf4e70dbeab324
Thomas Whittle <tewhittle@gmail.com>
2021-01-12 15:13:30 -0500

    Establish notes api posting functionality

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const path = require("path");
+const db = require("./db/db.json");
 const PORT = process.env.PORT || 3000;
-const data = require("./db/db")
 
 
 //middleware
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+
 
 //HTML routes
 app.get("/", function(req, res) {
@@ -21,28 +23,35 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
 
+
+
 //API routes
 app.get("/api/notes", function(req, res) 
 {
-    res.json(data)
+    res.sendFile(path.join(__dirname, "/db/db.json"));
 })
 
-app.post("/api/notes", function(req, res)
-{
+app.post("/api/notes", function(req, res) 
+ {
+    const notesList = JSON.parse(fs.readFileSync("./db/db.json"));
+    const postedNote = req.body;
+    notesList.push(postedNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesList))
+    res.json(notesList);
     
 })
-
-
-
-
-
-
-
-    
-
 
 //Server listen
 app.listen(PORT, function() 
 {
     console.log(`App listening on PORT: ${PORT}`);
 })
+    
+
+
+
+
+
+
+
+    
